5a9111a2dadcf63be4cc53762c60c6489d2b2e16
bmeneghini <bmuschioni@gmail.com>
2018-08-15 12:55:32 -0300

    Initial commit

---
+++ src/main/java/com/example/move2playstoreapi/Move2playStoreApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.move2playstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Move2playStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Move2playStoreApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/move2playstoreapi/Move2playStoreApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.move2playstoreapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Move2playStoreApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
